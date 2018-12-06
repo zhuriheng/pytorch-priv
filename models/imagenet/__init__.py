0cb2b35d2e30a04a9266a13c4d26de026ccf3dce
zhuriheng <zrh0712@outlook.com>
2018-12-06 13:32:26 +0800

    add dropblock module

--- models/imagenet/__init__.py
+++ models/imagenet/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-from .air import *
-from .airx import *
+#from .air import *
+#from .airx import *
 from .densenet import *
 from .mobilenet import *
 from .preresnet import *
@@ -9,10 +9,11 @@ from .resnet import *
 from .resnext import *
 from .shufflenet import *
 from .vgg import *
-from .se_air import *
-from .se_airx import *
+#from .se_air import *
+#from .se_airx import *
 from .se_densenet import *
 from .se_mobilenet import *
 from .se_resnet import *
 from .se_resnext import *
 from .se_shufflenet import *
+from .resdropnet import *
